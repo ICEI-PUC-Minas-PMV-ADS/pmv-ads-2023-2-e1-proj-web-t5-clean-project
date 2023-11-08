d098e16202c15f50f6d92909489252b50a2bd3e0
Felipe <felipedante157@gmail.com>
2023-11-08 20:04:11 -0300

    Parte de cadastro da Maju

--- codigo-fonte/js/cadastro.js
+++ codigo-fonte/js/cadastro.js
@@ -1,1 +1,24 @@
+var btnSignin = document.querySelector("#signin");
+var btnSignup = document.querySelector("#signup");
 
+var body = document.querySelector("body");
+
+
+btnSignin.addEventListener("click", function () {
+   body.className = "sign-in-js"; 
+});
+
+btnSignup.addEventListener("click", function () {
+    body.className = "sign-up-js";
+})
+
+const emailInput = document.querySelector('input[type="email"]');
+    const passwordInput = document.querySelector('input[type="password"]');
+    const form = document.querySelector('form');
+
+    form.addEventListener('submit', function (e) {
+        if (emailInput.value === '' || passwordInput.value === '') {
+            e.preventDefault(); // Impedir o envio do formulário
+            alert('Por favor, preencha todos os campos.');
+        }
+    });
